d26dc3a269f08faa2479af935e5f90b8809f14d9
lvxusNy <lvxus.ying@gmail.com>
2019-08-20 16:57:38 +0800

    lvxus

---
+++ kata/src/main/java/com/tdd/kata/Args.java
@@ -0,0 +1,7 @@
+package com.tdd.kata;
+
+public class Args {
+    public String getValue() {
+        return "1";
+    }
+}
--- kata/src/test/java/com/tdd/kata/ArgsTest.java
+++ kata/src/test/java/com/tdd/kata/ArgsTest.java
@@ -1,10 +1,13 @@
 package com.tdd.kata;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ArgsTest {
     @Test
     public void args_test() {
+        Args args = new Args();
+        Assert.assertEquals(args.getValue(),"1");
 
     }
 }
