d2a0448ef641022c0a3abb1994b75c662a0a2817
lvxusNy <lvxus.ying@gmail.com>
2019-08-20 17:28:09 +0800

    lvxus

---
+++ kata/src/main/java/com/tdd/kata/Args.java
@@ -0,0 +1,11 @@
+package com.tdd.kata;
+
+public class Args {
+    public Args(String schemas, String command) {
+
+    }
+
+    public String getValue(String flagName) {
+        return null;
+    }
+}
